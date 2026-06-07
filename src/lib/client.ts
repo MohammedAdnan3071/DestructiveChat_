634711fb9cbfa30898ebd3a03006fcf8e6d0237d
Mohammed Adnan <adnan160320733071@gmail.com>
2026-06-07 23:34:14 +0200

    updated treaty

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
-// src/lib/client.ts
+/* // src/lib/client.ts
 import { treaty } from '@elysiajs/eden';
 import type { App } from '../app/api/[[...slugs]]/route';
 
@@ -7,4 +7,9 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
 
 // export const client = treaty(API_URL).api;
 
-export const client = treaty<App>(API_URL)
+export const client = treaty<App>(API_URL) */
+
+
+// This is now just a placeholder – you won't use treaty anymore
+export const client = null as any
+// Or remove this file and use fetch directly
